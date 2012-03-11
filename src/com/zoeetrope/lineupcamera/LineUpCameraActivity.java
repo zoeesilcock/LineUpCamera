f3df43f8764181533195cd92d6084a5dde24abf5
Zoee <mrzoee@gmail.com>
2012-03-11 10:29:33 +0100

    initial commit

---
+++ src/com/zoeetrope/lineupcamera/LineUpCameraActivity.java
@@ -0,0 +1,13 @@
+package com.zoeetrope.lineupcamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LineUpCameraActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
