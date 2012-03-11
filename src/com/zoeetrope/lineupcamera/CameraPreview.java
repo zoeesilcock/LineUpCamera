f4893b91a20c53b1ec38b70aa4e6ad55905e02ff
Zoee <mrzoee@gmail.com>
2012-03-11 15:55:33 +0100

    the preview orientation is now updated correctly

--- src/com/zoeetrope/lineupcamera/CameraPreview.java
+++ src/com/zoeetrope/lineupcamera/CameraPreview.java
@@ -1,9 +1,13 @@
 package com.zoeetrope.lineupcamera;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.List;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.hardware.Camera;
+import android.os.Build;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
@@ -54,11 +58,46 @@ public class CameraPreview extends SurfaceView implements Callback {
 		}
 
 		try {
+			Camera.Parameters parameters = mCamera.getParameters();
+			List<Camera.Size> sizes = parameters.getSupportedPreviewSizes();
+			Camera.Size cs = sizes.get(0);
+			parameters.setPreviewSize(cs.width, cs.height);
+
+			if (Integer.parseInt(Build.VERSION.SDK) >= 8) {
+				if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+					setDisplayOrientation(mCamera, 90);
+				}
+				if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+					setDisplayOrientation(mCamera, 0);
+				}
+			} else {
+				if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+					parameters.set("orientation", "portrait");
+					parameters.set("rotation", 90);
+				}
+				if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+					parameters.set("orientation", "landscape");
+					parameters.set("rotation", 90);
+				}
+			}
+
 			mCamera.setPreviewDisplay(mHolder);
+			mCamera.setParameters(parameters);
 			mCamera.startPreview();
 			requestLayout();
 		} catch (Exception e) {
 			Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+		}
+	}
+
+	protected void setDisplayOrientation(Camera camera, int angle) {
+		Method downPolymorphic;
+		try {
+			downPolymorphic = camera.getClass().getMethod(
+					"setDisplayOrientation", new Class[] { int.class });
+			if (downPolymorphic != null)
+				downPolymorphic.invoke(camera, new Object[] { angle });
+		} catch (Exception e1) {
 		}
 	}
 
