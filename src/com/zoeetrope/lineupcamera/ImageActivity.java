0ebb2db4eb37bf601ed05eb791d66eba44ed3de6
Zoee <mrzoee@gmail.com>
2012-05-18 12:10:07 +0200

    make the image viewer fullscreen with no title bar

--- src/com/zoeetrope/lineupcamera/ImageActivity.java
+++ src/com/zoeetrope/lineupcamera/ImageActivity.java
@@ -12,6 +12,8 @@ import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.zoeetrope.lineupcamera.model.Album;
@@ -39,6 +41,9 @@ public class ImageActivity extends Activity implements OnTouchListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		setContentView(R.layout.image);
 		mImageView = (ImageView) findViewById(R.id.image);
